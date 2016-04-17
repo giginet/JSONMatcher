3edb5c249b2db101253fd69c99653dd1fecb74bd
giginet <giginet.net@gmail.com>
2016-04-17 14:39:22 +0900

    Initial Commit

---
+++ Sources/JSONMatcher/JSONMatcher.h
@@ -0,0 +1,19 @@
+//
+//  JSONMatcher.h
+//  JSONMatcher
+//
+//  Created by giginet on 4/17/16.
+//  Copyright © 2016 giginet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONMatcher.
+FOUNDATION_EXPORT double JSONMatcherVersionNumber;
+
+//! Project version string for JSONMatcher.
+FOUNDATION_EXPORT const unsigned char JSONMatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONMatcher/PublicHeader.h>
+
+

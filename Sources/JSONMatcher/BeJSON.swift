bac97fce6d8e78b2622f2aafc569a837cd824003
giginet <giginet.net@gmail.com>
2016-04-23 10:32:10 +0900

    Remove unused file

--- Sources/JSONMatcher/BeJSON.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public func beJSON<T>() -> MatcherFunc<T> {
-    return MatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "be nil"
-        let actualValue = try actualExpression.evaluate()
-        return actualValue == nil
-    }
-}
--- Sources/JSONMatcher/Matcher/BeJSONAs.swift
+++ Sources/JSONMatcher/Matcher/BeJSONAs.swift
@@ -1,6 +1,5 @@
 import Foundation
 import Nimble
-import SwiftyJSON
 
 public func beJSONAs<T>(expected: Any) -> MatcherFunc<T> {
     return MatcherFunc { actualExpression, failureMessage in

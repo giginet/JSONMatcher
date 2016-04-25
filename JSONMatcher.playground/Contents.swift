b4bc4e2563dba297dda63bc51ddd0d87071075a4
giginet <giginet.net@gmail.com>
2016-04-25 19:44:50 +0900

    Implement new JSONElement

--- JSONMatcher.playground/Contents.swift
+++ JSONMatcher.playground/Contents.swift
@@ -4,73 +4,18 @@ import UIKit
 import SwiftyJSON
 @testable import JSONMatcher
 
-let data = "{".dataUsingEncoding(NSUTF8StringEncoding)!
-let json = JSON(data: data)
-json.isEmpty
-json.isExists()
-json.null
+let builder = Builder()
+builder.buildJSONElement(["a", "b"])
+builder.buildJSONElement(1)
+builder.buildJSONElement(true)
+let arrayElement: ArrayElement = builder.buildJSONElement(["a", "b", ["a", "b", ["a", "b"]]]) as! ArrayElement
+arrayElement.value[0]
+arrayElement.value[1]
+let innerArrayElement: ArrayElement = arrayElement.value[2] as! ArrayElement
+innerArrayElement.value[0]
+innerArrayElement.value[1]
+innerArrayElement.value[2]
 
-let ac: AnyObject.Type = UIView.self
-"hoge".dynamicType
-
-NSStringFromClass(ac)
-
-ObjectIdentifier(ac) == ObjectIdentifier("".dynamicType)
-
-let string: JSONElement = "foo"
-let element = JSONElement("foo")
-
-let elementArray: JSONElement = [
-    "foo",
-    "bar",
-    ["hoge", 10, 30],
-    [
-        "key" : "value",
-        "tags" : ["a", "b", "c"]
-    ],
-]
-
-/*let elementDictionary = [
-    "piyo" : RegexElement(".+"),
-    "type" : TypeElement(String.self)
-]*/
-
-Comparer.compare(["foo", 10, 20], ["foo", 10, 20])
-
-["a", 10].dynamicType
-
-protocol AnyProtocol { }
-
-0x10 == 16
-
-[10, "^foo".regex, "bar", "[a-z]+"]
-
-protocol Test { }
-extension String: Test { }
-extension Int: Test { }
-extension NSObject: Test { }
-
-let stringArray: [String] = ["a", "b", "c"]
-"a" is Test
-//stringArray is [Test]
-
-func compareArray<T: CollectionType where T.Generator.Element == Test>(lhs: T) -> Bool {
-    return true
-}
-
-func compareDictionary<T: CollectionType where T.Generator.Element == (String, Test)>(lhs: T) {
-}
-
-let list: [Test] = [1, 2, "aaa"]
-compareArray(list)
-
-func testFunction(lhs: AcceptableValueType, _ rhs: AcceptableValueType) -> Bool {
-    switch (lhs, rhs) {
-    case let (number as String, expectedNumber as String):
-        return number == expectedNumber
-    default:
-        return false
-    }
-    return false
-}
-testFunction("a", "a")
+let dictionaryElement = builder.buildJSONElement(["name": "hoge", "items": ["a", "b", 10, 100, true]]) as! DictionaryElement
+dictionaryElement.value["name"]
+dictionaryElement.value["items"]
---
+++ Sources/JSONMatcher/Builder.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+struct Builder {
+    func buildJSONElement(object: AnyObject) -> AbstractJSONElementType {
+        switch object {
+        case let array as NSArray:
+            let array = buildJSONElementArray(array)
+            return ArrayElement(array)
+        case let dictionary as NSDictionary:
+            let dictionary = buildJSONElementDictionary(dictionary)
+            return DictionaryElement(dictionary)
+        default:
+            return buildRawJSONElement(object)
+        }
+    }
+    
+    func buildRawJSONElement(object: AnyObject) -> AbstractJSONElementType {
+        switch object {
+        case let int as Int:
+            let number = NSNumber(integer: int)
+            return NumberElement(number)
+        case let int as Double:
+            let number = NSNumber(double: int)
+            return NumberElement(number)
+        case let string as String:
+            return StringElement(string)
+        case let bool as Bool:
+            return BoolElement(bool)
+        case let null as NSNull:
+            return NullElement(null)
+        default:
+            return NullElement(NSNull())
+        }
+    }
+    
+    func buildJSONElementArray(array: NSArray) -> [AbstractJSONElementType] {
+        var result: [AbstractJSONElementType] = []
+        for element in array {
+            if let innerRawArray = element as? NSArray {
+                let innerArray = buildJSONElementArray(innerRawArray)
+                let arrayElement = ArrayElement(innerArray)
+                result.append(arrayElement)
+            } else if let innerRawDictionary = element as? NSDictionary {
+                let innerDictionary = buildJSONElementDictionary(innerRawDictionary)
+                let dictionaryElement = DictionaryElement(innerDictionary)
+                result.append(dictionaryElement)
+            } else {
+                let rawElement = buildRawJSONElement(element)
+                result.append(rawElement)
+            }
+        }
+        return result
+    }
+    
+    func buildJSONElementDictionary(dictionary: NSDictionary) -> [String: AbstractJSONElementType] {
+        var result: [String: AbstractJSONElementType] = [:]
+        for (k, v) in dictionary {
+            guard let k = k as? String else {
+                continue
+            }
+            if let innerRawArray = v as? NSArray {
+              let innerArray = buildJSONElementArray(innerRawArray)
+                let arrayElement = ArrayElement(innerArray)
+                result[k] = arrayElement
+            } else if let innerRawDictionary = v as? NSDictionary {
+                let innerDictionary = buildJSONElementDictionary(innerRawDictionary)
+                let dictionaryElement = DictionaryElement(innerDictionary)
+                result[k] = dictionaryElement
+            } else {
+                let rawElement = buildRawJSONElement(v)
+                result[k] = rawElement
+            }
+        }
+        return result
+    }
+}

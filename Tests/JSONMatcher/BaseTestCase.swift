9bf542b47bc9c0c8f30278a9f2dd4559469e814a
Boris Dušek <dusek@a11y.ltd.uk>
2017-04-29 15:08:15 +0200

    Review comment: adhere to Swift API guidelines

--- Tests/JSONMatcher/BaseTestCase.swift
+++ Tests/JSONMatcher/BaseTestCase.swift
@@ -2,9 +2,9 @@ import Foundation
 import XCTest
 
 class BaseTestCase: XCTestCase {
-    func loadJSONFile(_ JSONName: String) -> String {
+    func loadJSONFile(_ jsonName: String) -> String {
         let bundle = Bundle(for: type(of: self))
-        let path = bundle.path(forResource: JSONName, ofType: "json")!
+        let path = bundle.path(forResource: jsonName, ofType: "json")!
         let jsonData = try? Data(contentsOf: URL(fileURLWithPath: path))
         return String(data: jsonData!, encoding: .utf8)!
     }
